c7fc0a10c22ca1391d9495d2b68a68ed62ff2c9e
N.Mice <n.mice@mail.ru>
2022-05-23 01:13:14 +0300

    hw00-init project repo

---
+++ src/main/java/ru/neginskiy/familytime/FamilytimeApplication.java
@@ -0,0 +1,13 @@
+package ru.neginskiy.familytime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilytimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FamilytimeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/neginskiy/familytime/FamilytimeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.neginskiy.familytime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilytimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
